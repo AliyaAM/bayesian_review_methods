cfd158c36be88436d7b8a08923b687cecb16b559
AliyaAM <aliya.amirova005@gmail.com>
2023-03-10 08:19:57 +0000

    Create violin_plot.R

---
+++ violin_plot.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(dplyr)
+library(survminer)
+########### DIRECTORY
+
+#directory = "/Users/aliyaamirova/"
+directory = "/Users/aliya/my_docs/"
+
+###########  source root 
+SOURCE_ROOT = paste(directory, "proj/bayesian_review_methods/", sep = "")
+
+###########  data root
+DATA_ROOT = paste(directory, "proj/bayesian_review_methods/DATA/", sep = "")
+
+########### Set the root location on the user's local machine to save output files.
+OUTPUT_ROOT = paste(directory, "proj/bayesian_review_methods/RESULTS/CHatGPTvsHuman/Mean_percentage_quote/", sep = "")
+
+
+
+#DATA: 
+data_per_domain_per_case = read.csv(paste(DATA_ROOT, "data_per_domain_per_participant.csv", sep =""))
+
