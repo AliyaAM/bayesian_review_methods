2ce59d1dab446a194df8efadbf6247b1d8e5a348
AliyaAM <aliya.amirova005@gmail.com>
2023-03-10 04:33:32 +0000

    Update plotting_quotes_n_ChatGPTvsHuman.R

--- plotting_quotes_n_ChatGPTvsHuman.R
+++ plotting_quotes_n_ChatGPTvsHuman.R
@@ -1,6 +1,7 @@
 
 
-
+library(ggplot2)
+library(survminer)
 ########### DIRECTORY
 
 #directory = "/Users/aliyaamirova/"
@@ -32,18 +33,19 @@ Active_inactive_ChatGPT = subset(Active_inactive_ChatGPT, belief_statement !="To
 construct = c(Active_inactive_Human$construct, Active_inactive_Human$construct)
 data_human = data.frame(construct)
 data_human$quotes = c(Active_inactive_Human$active_k, Active_inactive_Human$sedentary_k)
-active_status = rep(1, times= nrow(Active_inactive_Human))
-sedentary_status = rep(0, times= nrow(Active_inactive_Human))
-data_human$PA_status = as.factor(c(active_status, sedentary_status))
-data_human$human = rep(1, times= nrow(data_human))
+active_status = rep("Active", times= nrow(Active_inactive_Human))
+sedentary_status = rep("Sedentary", times= nrow(Active_inactive_Human))
+data_human$PA_status = c(active_status, sedentary_status)
+data_human$human = rep("Human participants", times= nrow(data_human))
 data_human = data_human[complete.cases(data_human), ]
+
 construct = c(Active_inactive_ChatGPT$construct, Active_inactive_ChatGPT$construct)
 data_GPT = data.frame(construct)
 data_GPT$quotes = c(Active_inactive_ChatGPT$active_k, Active_inactive_ChatGPT$sedentary_k)
-active_status = rep(1, times= nrow(Active_inactive_ChatGPT))
-sedentary_status = rep(0, times= nrow(Active_inactive_ChatGPT))
-data_GPT$PA_status = as.factor(c(active_status, sedentary_status))
-data_GPT$human = rep(0, times = nrow(data_GPT))
+active_status = rep("Active", times= nrow(Active_inactive_ChatGPT))
+sedentary_status = rep("Sedentary", times= nrow(Active_inactive_ChatGPT))
+data_GPT$PA_status = c(active_status, sedentary_status)
+data_GPT$human = rep("Chat GPT characters", times = nrow(data_GPT))
 data_GPT = data_GPT[complete.cases(data_GPT), ]
 data = rbind(data_human, data_GPT)
 
@@ -72,6 +74,69 @@ data_domains = subset(data, data$construct %in% c(construct_list_enablers))
 Enablers_plot = ggplot(data_domains, aes(y= reorder(construct, +quotes), x = quotes)) +
                        geom_col(aes(fill = PA_status), position = "dodge") + 
                        facet_wrap(~human, nrow=1)
+
+
+Enablers_plot = Enablers_plot + scale_y_discrete(labels=c("BR+" = "Behavioural regulation", 
+                                                          "BaCap+" = "Beliefs about capabilities", 
+                                                          "BaCon+" = "Beliefs about consequences", 
+                                                          "ECR+" = "Environmental context and resources", 
+                                                          "Emotion+" = "Emotion", 
+                                                          "Goals+" = "Goals", 
+                                                          "Intention+" = "Intentions", 
+                                                          "Knowledge+" = "Knowledge", 
+                                                          "MADP+" = "Memory, attention, and decision processes", 
+                                                          "Optimism+" = "Optimism", 
+                                                          "Reinforcement+" = "Reinforcement", 
+                                                          "SI+" = "Social influences", 
+                                                          "\nSPR+" = "Social, professional role and identity", 
+                                                          "Skills+" = "Skills"
+                                                          )) 
+
+
+# changing palette 
+Enablers_plot = Enablers_plot + scale_fill_brewer(palette = "Set1")+ scale_color_brewer(palette = "Set1")
+
+#labeling axis
+Enablers_plot = Enablers_plot + ggtitle("Enablers") + xlab("quotes, k") + ylab("Domain")
+
+#labeling legend
+Enablers_plot = Enablers_plot + guides(fill=guide_legend(title=""))
+
+
+#perform t-test for each category
+
+# human
+data_human = subset(data_domains, human == "Human participants")
+
+compare_means(quotes ~ PA_status,  data = data_human,
+              ref.group = "Sedentary", method = "t.test")
+
+
+# ChatGPT
+data_ChatGPT = subset(data_domains, human == "Chat GPT characters")
+
+compare_means(quotes ~ PA_status,  data = data_ChatGPT,
+              ref.group = "Sedentary", method = "t.test")
+
+
+# compare_means(quotes ~ PA_status,  data = data_domains,
+#               ref.group = "Sedentary", method = "t.test")
+# 
+# 
+# Enablers_plot = Enablers_plot +   
+#   stat_compare_means(label = "p.signif", method = "t.test",
+#                      ref.group = ".all.")   
+# 
+# Enablers_plot
+# print(Enablers_plot)
+
+# #adding sig diff test 
+# Enablers_plot = Enablers_plot + geom_signif(stat="identity",
+#                 data=data.frame(x=c(0.875, 1.875), xend=c(1.125, 2.125),
+#                                   y=c(5.8, 8.5), annotation=c("**", "NS")),
+#                 aes(x=x,xend=xend, y=y, yend=y, annotation=annotation))
+
+
 
 ggsave(file = paste(OUTPUT_ROOT, "/Enablers_plot.pdf",  sep=""), Enablers_plot, 
        #width=6, height=2, units="in", 
@@ -114,6 +179,27 @@ Barriers_plot = ggplot(data_domains_barriers, aes(y= reorder(construct, +quotes)
                        geom_col(aes(fill = PA_status), position = "dodge") + 
                        facet_wrap(~human, nrow=1)
 
+
+# The TDF domains and their descriptors are outlined in Table 1; the 14 domains are 1) knowledge, 2) skills, 3) social/professional role and identity, 4) beliefs about capabilities, 5) optimism, 6) beliefs about consequences, 7) reinforcement, 8) intentions, 9) goals, 10) memory, attention, and decision processes, 11) environment context and resources, 12) social influences, 13) emotion, and 14) behavioral regulation
+
+Barriers_plot = Barriers_plot + scale_y_discrete(labels=c("BR-" = "Behavioural regulation", 
+                                                          "BaCap-" = "Beliefs about capabilities", 
+                                                          "BaCon-" = "Beliefs about consequences", 
+                                                          "ECR-" = "Environmental context and resources", 
+                                                          "Emotion-" = "Emotion", 
+                                                          "Goals-" = "Goals", 
+                                                          "Intention-" = "Intentions", 
+                                                          "Knowledge-" = "Knowledge", 
+                                                          "MADP-" = "Memory, attention, and decision processes", 
+                                                          "Optimism-" = "Optimism", 
+                                                          "Reinforcement-" = "Reinforcement", 
+                                                          "SI-" = "Social influences", 
+                                                          "SPR-" = "Social, professional role and identity", 
+                                                          "Skills-" = "Skills"
+                                                          ))
+Barriers_plot = Barriers_plot + scale_fill_brewer(palette = "Set1")+ scale_color_brewer(palette = "Set1")
+Barriers_plot = Barriers_plot + ggtitle("Barriers") + xlab("quotes, k") + ylab("Domain")
+Barriers_plot = Barriers_plot + guides(fill=guide_legend(title=""))
 
 ggsave(file = paste(OUTPUT_ROOT, "/Barriers_plot.pdf",  sep=""), Barriers_plot, 
        #width=6, height=2, units="in", 
